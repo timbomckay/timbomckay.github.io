afbe799c9edb279749a5712cf91834c78c97472a
Tim McKay <timbomckay@gmail.com>
2017-03-07 01:03:26 -0500

    Updating styles and portfolio pieces

--- js/agency.js
+++ js/agency.js
@@ -1,33 +1,70 @@
-// Agency Theme JavaScript
+// JavaScript
+/*global ga */
+
+function randomChoice(arr) {
+  return arr[Math.floor(arr.length * Math.random())];
+}
+
+function eventTracking(eDesc, action) {
+  if (action === undefined) {
+   action = "click";
+  }
+  ga('send', 'event', {
+    eventCategory: 'Outbound Link',
+    eventAction: action,
+    eventLabel: eDesc,
+    transport: 'beacon'
+  });
+}
 
 (function($) {
-    "use strict"; // Start of use strict
+  "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  // Random Background Image
+  var bgImages = [
+    'TCCCKgdFmBo',
+    '0l0yzOD1ZDI',
+    'W3V2tQAZxPA',
+    'ZQwyFDqNxa0',
+    '9jYj32TN9Ts',
+    'cv_fqvrbKWs',
+    'jXyKqnFnZos'
+  ];
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+  $('header').css('background-image', 'url("https://source.unsplash.com/'+ randomChoice(bgImages) +'/1600x900/")');
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
-    });
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  // $('a.page-scroll').bind('click', function(event) {
+  //   var $anchor = $(this);
+  //   $('html, body').stop().animate({
+  //     scrollTop: ($($anchor.attr('href')).offset().top - 50)
+  //   }, 1250, 'easeInOutExpo');
+  //   event.preventDefault();
+  // });
+
+  // Highlight the top nav as scrolling occurs
+  // $('body').scrollspy({
+  //   target: '.navbar-fixed-top',
+  //   offset: 51
+  // });
+
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function(){
+    $('.navbar-toggle:visible').click();
+  });
+
+  // Offset for Main Navigation
+  $('#mainNav').affix({
+    offset: {
+      top: 100
+    }
+  });
+
+  $('.portfolio-modal').on('shown.bs.modal', function () {
+
+    eventTracking( $(this).data('label') );
+
+  });
 
 })(jQuery); // End of use strict
