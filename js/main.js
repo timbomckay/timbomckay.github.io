821e1b5f7b7dee31c9ada7530679261d5ed5d1a9
Tim McKay <timbomckay@gmail.com>
2018-06-04 22:31:48 -0400

    Modal + Content

--- js/main.js
+++ js/main.js
@@ -1,34 +1,41 @@
 // JavaScript
-/*global ga */
+/*global ga, Vue */
 
-function eventTracking(eDesc, action) {
-  if (action === undefined) {
-   action = "click";
-  }
-  ga('send', 'event', {
-    eventCategory: 'Outbound Link',
-    eventAction: action,
-    eventLabel: eDesc,
-    transport: 'beacon'
-  });
-}
+// register modal component
+Vue.component('modal', {
+  template: '#modal-template'
+})
 
-(function($) {
-  "use strict"; // Start of use strict
+Vue.config.devtools = true
 
-  $('.portfolio-modal').on('shown.bs.modal', function () {
-
-    eventTracking( $(this).data('project'), 'View Modal' );
-
-  });
-
-  $('a[target="_new"]').click(function() {
-    if( $(this).data('project') ) {
-      eventTracking( $(this).data('project') + ' view', 'View Modal' );
-    } else {
-      eventTracking( $(this).closest('.portfolio-modal').data('project') + ' visit', 'Visit Project' );
+// start app
+new Vue({
+  el: '#app',
+  mounted: function() {
+    let self = this
+    fetch('dist/entries.min.json')
+    .then(response => {
+      response.json().then(function(data) {
+        self.entries = data
+      })
+    })
+  },
+  data: {
+    active: false,
+    entries: false
+  },
+  methods: {
+    trackEvent: function(desc, event) {
+      ga('send', 'event', {
+        eventCategory: 'Outbound Link',
+        eventAction: event || 'click',
+        eventLabel: desc,
+        transport: 'beacon'
+      });
+    },
+    showModal: function(entry) {
+      this.active = this.entries[entry];
+      this.trackEvent(entry, 'View Project')
     }
-
-  });
-
-})(jQuery); // End of use strict
+  }
+})
